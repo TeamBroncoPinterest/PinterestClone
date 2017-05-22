57982c75b063e7d2d95e5a6495272c183d6cc3e0
Cameron <whatiscameronsemail@gmail.com>
2017-05-22 11:34:37 -0600

    laying out the initial structure and providing an example of redux.

---
+++ client/src/containers/SinglePin.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { createUser } from '../actions/userActions';
+
+class SinglePin extends Component {
+
+  render() {
+    // const currentUser = this.props.currentUser;
+    // this.props.createUser(email, password);
+    return (
+      <div className="single-pin-outer-container">
+        <div className="single-pin-inner-container">
+          <div className="single-pin-pic-container">
+            <img className="single-pin-pic" src="./pics/education4.png" alt="pin picture" />
+            <button className="single-pin-save"><img className="single-pin-save-pin" src="./pics/pin.png" alt="pin"/>Save</button>
+            <div className="dimGradient">
+              <a><p>Learn more at <span>link.com</span></p></a>
+            </div>
+            <div className="dimOverlay"></div>
+          </div>
+          <div className="single-pin-description-container">
+            <p className="single-pin-title">
+              Title of Pin
+            </p>
+            <p className="single-pin-description">
+              Pin description right here. Lorem ipsum dolor sit amet, consectetur
+              adipisicing elit.
+            </p>
+          </div>
+          <div className="single-pin-pinner-info">
+            <img className="single-pin-pinner-pic" src="./pics/blank-profile.png" alt="pic"/>
+            <div className="single-pin-pinner-info-names">
+              <p className="single-pin-pinner-name">Pinner Name</p>
+              <p className="single-pin-board-name">Board Name</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+function mapStateToProps(store) {
+  return {
+    currentUser: store.user.currentUser
+  }
+}
+
+
+export default connect(mapStateToProps, { createUser })(SinglePin);

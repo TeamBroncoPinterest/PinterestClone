a7867620bcd2d9e6c9c32367ce4a926cf489fc00
Seth Rumbaugh <sethrumbaugh@gmail.com>
2017-05-24 11:33:50 -0600

    Modal update

--- client/src/containers/CreateBoardModal_Insert.js
+++ client/src/containers/CreateBoardModal_Insert.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Modal_Component from "./Modal_Component"
 
 
 class CreateBoard extends Component {
@@ -6,12 +7,20 @@ class CreateBoard extends Component {
   render() {
     return (
 
+        <Modal_Component title="Create board">
 
-              <div  className="CreateBoard_SecondBox">
-                <h5 className="CreateBoard_Name">Name</h5>
-                <input  type='text' className="CreateBoard_Input" placeholder='Like "places to Go" or "Recipes to Make"' />
-              </div>
+          <div  className="CreateBoard_CreateBoard">
+            <h5 className="CreateBoard_Name">Name</h5>
+              <input  type='text' className="CreateBoard_Input" placeholder='Like "places to Go" or "Recipes to Make"' require />
+            </div>
 
+
+            <div  className="CreateBoard_CreateBoard">
+             <h5 className="CreateBoard_Name">Description</h5>
+             <textarea  type='text'  className="CreateBoard_Input"  required> </textarea>
+           </div>
+
+        </Modal_Component>
 
     )
   }
--- client/src/containers/Modal_Component.js
+++ client/src/containers/Modal_Component.js
@@ -1,29 +1,34 @@
 import React from 'react';
 
+
+
 class Modal_Component extends Component {
 
   render() {
     return (
       <div>
-        <div className="ModalBoard_wrapper">
-          <div className="ModalBoard_Box">
+            <div className="ModalBoard_wrapper">
+              <div className="ModalBoard_Box">
 
-            <div className="ModalBoard_TopBox">
-              <p>Future Dynamic Name Here</p>
-            </div>
+                <div className="ModalBoard_TopBox">
+                  <p className="Modal_title">{ this.props.title }</p>
+                  <div className="ModalBoard_X"></div>
+                </div>
 
-            OTHER MODAL COMPONENTS HERE
+                { this.props.children }
 
-            <div  className="ModalBoard_ButtonBox">
+                   
 
-              <div class="Buttons_Component">
-                  <button className="cancel_button"><b>Cancel</b></button>
-                <button className="create_button"><b>Create</b></button>
-             </div>
+                <div  className="ModalBoard_ButtonBox">
+
+                  <div className="Buttons_Component">
+                      <button className="cancel_button"><b>Cancel</b></button>
+                    <button className="create_button"><b>Create</b></button>
+                 </div>
+                 </div>
+               </div>
              </div>
            </div>
-         </div>
-       </div>
     )
   }
 }

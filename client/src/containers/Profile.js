f9d72f10b59aaabcd726a333f0a501843d8f2e4a
Cameron <whatiscameronsemail@gmail.com>
2017-05-25 15:30:02 -0600

    adding more styling on Profile page

--- client/src/containers/Profile.js
+++ client/src/containers/Profile.js
@@ -1,20 +1,49 @@
 import React, { Component } from 'react';
 
 import './Profile.css';
-
+import nut from '../pics/nut.png';
+import dotdotdot from '../pics/dotdotdot.png';
+import blankProfile from '../pics/blank-profile.png';
 
 class Profile extends Component {
   render() {
+    const data = {
+      first: 'Cameron',
+      bio: 'I <3 Pinterest',
+      img: blankProfile,
+      boards: []
+    }
+
     return (
       <div className="profile-container">
         <div className="profile-nav">
-          <div className="profile-side-margin">subnav</div>
+          <div className="profile-side-margin">
+            <div className="profile-icon">
+              <img className="profile-nut" src={nut} alt="" />
+            </div>
+            <div className="profile-icon">
+              <img className="profile-dotdotdot" src={dotdotdot} alt="" />
+            </div>
+          </div>
         </div>
         <div className="profile-profile-info">
-          <div className="profile-side-margin">info</div>
+          <div className="profile-side-margin profile-info-margin">
+            <div className="profile-name">
+              <h3>{data.first}</h3>
+            </div>
+            <div className="profile-profile-info-right" >
+              <div className="profile-info-bio">{data.bio}</div>
+              <div className="profile-info-pic-container">
+                <img className="profile-info-pic"src={data.img} alt="missing profile pic" />
+              </div>
+            </div>
+          </div>
         </div>
         <div className="profile-filter">
-          <div className="profile-side-margin">filter</div>
+          <div className="profile-side-margin profile-buttons">
+            <button className="profile-button profile-button-boards" >Boards</button>
+            <button className="profile-button profile-button-pins" >Pins</button>
+          </div>
         </div>
         <div className="profile-boards-pins">
           boards

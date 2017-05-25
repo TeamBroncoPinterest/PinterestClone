0ba944e25a03d9797574687efe98f26a66e0a66b
Cameron <whatiscameronsemail@gmail.com>
2017-05-25 12:31:28 -0600

    added initial elements and styling

--- client/src/containers/Profile.js
+++ client/src/containers/Profile.js
@@ -6,7 +6,20 @@ import './Profile.css';
 class Profile extends Component {
   render() {
     return (
-      <div>Profile</div>
+      <div className="profile-container">
+        <div className="profile-nav">
+          <div className="profile-side-margin">subnav</div>
+        </div>
+        <div className="profile-profile-info">
+          <div className="profile-side-margin">info</div>
+        </div>
+        <div className="profile-filter">
+          <div className="profile-side-margin">filter</div>
+        </div>
+        <div className="profile-boards-pins">
+          boards
+        </div>
+      </div>
     )
   }
 }

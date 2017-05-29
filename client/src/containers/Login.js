ecc819758792888a3b07ec69035672559d49d63b
Seth Rumbaugh <sethrumbaugh@gmail.com>
2017-05-29 09:36:14 -0600

    connect modal to appropiate div

--- client/src/containers/Login.js
+++ client/src/containers/Login.js
@@ -1,22 +1,9 @@
 import React, { Component } from 'react';
 
 import './Login.css';
-import CreateBoard from './CreateBoard';
 
 
 class Login extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { create: false }
-  }
-
-  createBoard = () => {
-    this.setState({ create: true })
-  }
-
-  closeWindow = () => {
-    this.setState({ create: false })
-  }
 
   render() {
 
@@ -24,8 +11,7 @@ class Login extends Component {
 
     return (
       <div className="login-container">
-        { this.state.create && <CreateBoard closeWindow={() => this.closeWindow()} /> }
-        <button onClick={() => this.createBoard()}>Login</button>
+        <button>Login</button>
 
       </div>
     )

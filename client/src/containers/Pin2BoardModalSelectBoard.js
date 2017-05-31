32cb1a09da5f8970ce4416afb3f4cc0b26117930
Kaleigh Niemela <nkaleigh@users.noreply.github.com>
2017-05-31 10:00:33 -0600

    board div

--- client/src/containers/Pin2BoardModalSelectBoard.js
+++ client/src/containers/Pin2BoardModalSelectBoard.js
@@ -1,50 +1,60 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
 
 import './Pin2Board.css'
 
+import BoardDiv from './BoardDiv';
+
 class Pin2BoardSelectBoard extends Component {
 
-  render() {
-    return (
+    render() {
 
-        <div className="Pin2Board_rightBox">
-            <div>
-                <h3 className="Pin2Board_ChooseBoard">Choose board</h3>
-                <div className="Pin2Board_X" onClick={() => this.props.savePinExit()}></div>
+        let boards = <div></div>
+        if (this.props.userBoards.data.boards) {
+            boards = this.props.userBoards.data.boards.map((val, ind) => {
+                    return <BoardDiv data={val} key={val.name} editBoard={() => this.editBoard()}/>
+                })
+        }
+        return (
+
+            <div className="Pin2Board_rightBox">
+                <div>
+                    <h3 className="Pin2Board_ChooseBoard">Choose board</h3>
+                    <div className="Pin2Board_X" onClick={() => this.props.savePinExit()}></div>
+
+                </div>
+
+                <div className="Pin2Board_LowerRight">
+                    <div className="Pin2Board_SearchDiv">
+                        <div className="Pin2Board_SearchMag"></div>
+                        <input type="text" placeholder="Search" className="Pin2Board_SearchBar"/>
+                    </div>
+                </div>
+
+                <div className="Pin2Board_BoardScrollBox">
+                    <header className="Pin2Board_Header">Existing boards</header>
+                    <ul className="Pin2Board_UL">
+                        {boards}
+                    </ul>
+                </div>
+
+                <button
+                    onClick={() => this.props.createBoardClick()}
+                    className="Pin2Board_CreateBoardButton"><img
+                    src="https://image.ibb.co/nH2FSa/Create_Board_Plus1.png"
+                    alt=""
+                    className="Pin2Board_PlusButton"/>
+                    <p className="Pin2Board_CreateBoard">Create board</p>
+                </button>
 
             </div>
 
-            <div className="Pin2Board_LowerRight">
-                <div className="Pin2Board_SearchDiv">
-                    <div className="Pin2Board_SearchMag"></div>
-                  <input type="text" placeholder="Search" className="Pin2Board_SearchBar" />
-                </div>
-           </div>
-
-          <div className="Pin2Board_BoardScrollBox">
-            <header className="Pin2Board_Header">Existing boards</header>
-                <ul className="Pin2Board_UL">
-
-                        <div className="Pin2Board_BoardDiv">
-                              <div className="Pin2Board_BoardImgBox">
-                                  <img  className="Pin2Board_BoardImg" alt="" src="https://s-media-cache-ak0.pinimg.com/200x150/65/75/23/65752306e76d3f8e7b16371299f541da.jpg" />
-                              </div>
-                          <p className="Pin2Board_BoardTitle"><b>Food</b></p>
-                          <button className="Pin2Board_SaveButton"><img className="Pin2Board_PinIcon" alt="" src="https://image.ibb.co/bKxULv/pin.png" /> Save</button>
-                        </div>
-                </ul>
-          </div>
-
-          <button onClick={() => this.props.createBoardClick()} className="Pin2Board_CreateBoardButton"><img src="https://image.ibb.co/nH2FSa/Create_Board_Plus1.png" alt="" className="Pin2Board_PlusButton" />
-          <p className="Pin2Board_CreateBoard">Create board</p></button>
-
-      </div>
-
-  )
-  }
+        )
+    }
 }
 
+function mapStateToProps(store) {
+    return {userBoards: store.user}
+}
 
-
-
-export default Pin2BoardSelectBoard;
+export default connect(mapStateToProps)(Pin2BoardSelectBoard);
--- client/src/containers/Profile.js
+++ client/src/containers/Profile.js
@@ -51,71 +51,59 @@ class Profile extends Component {
       boards: []
     }
 
-    // const boards = data
-    //   .boards
-    //   .map((v) => {
-    //     return <SingleBoard data={v} key={v.name}/>
-    //   })
+    // const boards = data   .boards   .map((v) => {     return <SingleBoard
+    // data={v} key={v.name}/>   })
 
-    return (
-      <div className="profile-container">
-        <div className="profile-nav">
-          <div className="profile-side-margin">
-            <div className="profile-icon">
-              <img className="profile-nut" src={nut} alt=""/>
-            </div>
-            <div className="profile-icon">
-              <img className="profile-dotdotdot" src={dotdotdot} alt=""/>
-            </div>
+    let boards = <div></div>
+    if (this.props.user.data.boards) {
+      boards = this.props.user.data.boards.map((val, ind) => {return <SingleBoard data={val} key={val.name} editBoard={() => this.editBoard()}/>
+    })
+  }
+
+  return (
+    <div className="profile-container">
+      <div className="profile-nav">
+        <div className="profile-side-margin">
+          <div className="profile-icon">
+            <img className="profile-nut" src={nut} alt=""/>
           </div>
-        </div>
-        <div className="profile-profile-info">
-          <div className="profile-side-margin profile-info-margin">
-            <div className="profile-name">
-              <h3>{data.first}</h3>
-            </div>
-            <div className="profile-profile-info-right">
-              <div className="profile-info-bio">{data.bio}</div>
-              <div className="profile-info-pic-container">
-                <img className="profile-info-pic" src={data.img} alt="missing profile pic"/>
-              </div>
-            </div>
+          <div className="profile-icon">
+            <img className="profile-dotdotdot" src={dotdotdot} alt=""/>
           </div>
-        </div>
-        <div className="profile-filter">
-          <div className="profile-side-margin profile-buttons">
-            <button className="profile-button profile-button-boards">Boards</button>
-            <button className="profile-button profile-button-pins">Pins</button>
-          </div>
-        </div>
-        <div className="profile-boards-pins">
-          {this.state.create && <CreateBoard closeWindow={() => this.closeWindow()}/>}
-          {this.state.edit && <UpdateBoard closeEditWindow={() => this.closeEditWindow()}/>}
-          <AddBoard createBoard={() => this.createBoard()}/>
-
-          <SingleBoard editBoard={() => this.editBoard()}/>
-          <SingleBoard/>
-
-          <SingleBoard/>
-
-          <SingleBoard/>
-          <SingleBoard/>
-          <SingleBoard/>
-          <SingleBoard/>
-          <SingleBoard/>
-          <SingleBoard/>
-          <SingleBoard/>
-          <SingleBoard/>
-          <SingleBoard/>
-          <SingleBoard/>
         </div>
       </div>
-    )
-  }
+      <div className="profile-profile-info">
+        <div className="profile-side-margin profile-info-margin">
+          <div className="profile-name">
+            <h3>{data.first}</h3>
+          </div>
+          <div className="profile-profile-info-right">
+            <div className="profile-info-bio">{data.bio}</div>
+            <div className="profile-info-pic-container">
+              <img className="profile-info-pic" src={data.img} alt="missing profile pic"/>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="profile-filter">
+        <div className="profile-side-margin profile-buttons">
+          <button className="profile-button profile-button-boards">Boards</button>
+          <button className="profile-button profile-button-pins">Pins</button>
+        </div>
+      </div>
+      <div className="profile-boards-pins">
+        {this.state.create && <CreateBoard closeWindow={() => this.closeWindow()}/>}
+        {this.state.edit && <UpdateBoard closeEditWindow={() => this.closeEditWindow()}/>}
+        <AddBoard createBoard={() => this.createBoard()}/> 
+        {boards}
+      </div>
+    </div>
+  )
+}
 }
 
 function mapStateToProps(store) {
-  return {user: store.user}
+return {user: store.user}
 }
 
 export default connect(mapStateToProps)(Profile);

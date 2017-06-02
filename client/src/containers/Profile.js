49775d24cdfea58cb89f4bdf7f268db21bc402b5
Kaleigh Niemela <nkaleigh@users.noreply.github.com>
2017-06-02 14:24:54 -0600

    board pin display

--- client/src/containers/Profile.js
+++ client/src/containers/Profile.js
@@ -12,6 +12,7 @@ import nut from '../pics/nut.png';
 import dotdotdot from '../pics/dotdotdot.png';
 import blankProfile from '../pics/bananaLady.jpg';
 import AddBoard from './AddBoard';
+import SinglePin from './SinglePin';
 
 class Profile extends Component {
 
@@ -19,7 +20,8 @@ class Profile extends Component {
     super(props);
     this.state = {
       create: false,
-      edit: false
+      edit: false,
+      pins: false
     }
   }
 
@@ -39,6 +41,15 @@ class Profile extends Component {
     this.setState({edit: false})
   }
 
+  displayPins = () => {
+    this.setState({pins: true})
+  }
+
+  displayBoards = () => {
+    // console.log("displayBoards");
+    this.setState({pins: false})
+  }
+
   render() {
 
     if (!this.props.user.loggedIn) {
@@ -47,7 +58,7 @@ class Profile extends Component {
     const data = {
       first: this.props.user.data.first + ' ' + this.props.user.data.last,
       bio: this.props.user.data.bio,
-      img: blankProfile,
+      img: blankProfile
     }
 
     // const boards = data   .boards   .map((v) => {     return <SingleBoard
@@ -56,57 +67,73 @@ class Profile extends Component {
     let boards = <div></div>
     if (this.props.user.data.boards) {
       boards = this.props.user.data.boards.map((val, ind) => {
-        let boardPins = this.props.user.data.pins.filter( v => {
-          return v.board.name === val.name
+          let boardPins = this.props.user.data.pins.filter(v => {
+              return v.board.name === val.name
+            })
+          val.pins = boardPins
+          return <SingleBoard data={val} key={val.name} editBoard={() => this.editBoard()}/>
         })
-        val.pins = boardPins
-        return <SingleBoard data={val} key={val.name} editBoard={() => this.editBoard()}/>
-    })
-  }
-  return (
-    <div className="profile-container">
-      <div className="profile-nav">
-        <div className="profile-side-margin">
-          <div className="profile-icon">
-            <img className="profile-nut" src={nut} alt=""/>
-          </div>
-          <div className="profile-icon">
-            <img className="profile-dotdotdot" src={dotdotdot} alt=""/>
-          </div>
-        </div>
-      </div>
-      <div className="profile-profile-info">
-        <div className="profile-side-margin profile-info-margin">
-          <div className="profile-name">
-            <h3 className="profile-nameBold">{data.first}</h3>
-          </div>
-          <div className="profile-profile-info-right">
-            <div className="profile-info-bio">{data.bio}</div>
-            <div className="profile-info-pic-container">
-              <img className="profile-info-pic" src={data.img} alt="missing profile pic"/>
+    }
+
+    let pins = <div></div>
+    if (this.props.user.data.pins) {
+      pins = this.props.user.data.pins.map((val, ind) => {
+          return <SinglePin data={val} key={val.id}/>
+        })
+    }
+    return (
+      <div className="profile-container">
+        <div className="profile-nav">
+          <div className="profile-side-margin">
+            <div className="profile-icon">
+              <img className="profile-nut" src={nut} alt=""/>
+            </div>
+            <div className="profile-icon">
+              <img className="profile-dotdotdot" src={dotdotdot} alt=""/>
             </div>
           </div>
         </div>
-      </div>
-      <div className="profile-filter">
-        <div className="profile-side-margin profile-buttons">
-          <button className="profile-button profile-button-boards">Boards</button>
-          <button className="profile-button profile-button-pins">Pins</button>
+        <div className="profile-profile-info">
+          <div className="profile-side-margin profile-info-margin">
+            <div className="profile-name">
+              <h3 className="profile-nameBold">{data.first}</h3>
+            </div>
+            <div className="profile-profile-info-right">
+              <div className="profile-info-bio">{data.bio}</div>
+              <div className="profile-info-pic-container">
+                <img className="profile-info-pic" src={data.img} alt="missing profile pic"/>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="profile-filter">
+          <div className="profile-side-margin profile-buttons">
+            <button
+              className="profile-button profile-button-boards"
+              onClick={() => this.displayBoards()}>Boards</button>
+            <button
+              className="profile-button profile-button-pins"
+              onClick={() => this.displayPins()}>Pins</button>
+          </div>
+        </div>
+        <div className="profile-boards-pins">
+          {this.state.create && <CreateBoard closeWindow={() => this.closeWindow()}/>}
+          {this.state.edit && <UpdateBoard data={''} closeEditWindow={() => this.closeEditWindow()}/>}
+          {!this.state.pins && <AddBoard createBoard={() => this.createBoard()}/>}
+          {!this.state.pins && boards}
+        </div>
+        <div className="profile-pins-wrapper">
+          <div className="profile-pins">
+            {this.state.pins && pins}
+          </div>
         </div>
       </div>
-      <div className="profile-boards-pins">
-        {this.state.create && <CreateBoard closeWindow={() => this.closeWindow()}/>}
-        {this.state.edit && <UpdateBoard data={''} closeEditWindow={() => this.closeEditWindow()}/>}
-        <AddBoard createBoard={() => this.createBoard()}/>
-        {boards}
-      </div>
-    </div>
-  )
-}
+    )
+  }
 }
 
 function mapStateToProps(store) {
-return {user: store.user}
+  return {user: store.user}
 }
 
 export default connect(mapStateToProps)(Profile);

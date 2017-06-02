3cab39fdb4fb793abd77a2e726a8f973bca56f36
Seth Rumbaugh <sethrumbaugh@gmail.com>
2017-06-02 11:29:36 -0600

    linking to external pin site

--- client/src/containers/UserBoard.js
+++ client/src/containers/UserBoard.js
@@ -26,16 +26,17 @@ class UserBoard extends Component {
     const length = data.pins ? data.pins.length : 0;
     return (
       <div className="board-wrapper">
+      <div className="board-fixedBar">
+            <div className="board-iconDiv">
+              <button className="board-icons1"></button>
+              <button className="board-icons2"></button>
+              <button className="board-icons3"></button>
+            </div>
+      </div>
         <div className="board-mainContainer">
           <div className="board-barDiv">
 
-              <div className="board-fixedBar">
-                    <div className="board-iconDiv">
-                      <button className="board-icons1"></button>
-                      <button className="board-icons2"></button>
-                      <button className="board-icons3"></button>
-                    </div>
-              </div>
+
 
               <div className="board-header">
                   <h3 className="board-title">{data.name}</h3>
